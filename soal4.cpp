1c8eb7bb9bd25512847d138f392250a9a4189524
F4CODE-COM <febrianalkatammi21@gmail.com>
2026-04-15 21:42:02 +0700

    soal4.cpp

--- soal4.cpp
+++ soal4.cpp
@@ -1,1 +1,24 @@
+#include<conio.h>
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Input jumlah n: ";
+    cin >> n;
+    
+    cout << "H";
+    for (int i = 0; i < n; i++) {
+        cout << "o";
+    }
+    cout << "r";
+    for (int i = 0; i < n; i++) {
+        cout << "e";
+    }
+    for (int i = 0; i < n; i++) {
+        cout << "!";
+    }
+    
+    getch();
+    return 0;
+}
