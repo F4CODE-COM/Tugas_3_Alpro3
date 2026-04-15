02df70895c3c2a5e4d3a7b157d1e9bddc3f51f68
F4CODE-COM <febrianalkatammi21@gmail.com>
2026-04-15 14:23:16 +0700

    soal1.cpp

--- soal1.cpp
+++ soal1.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+
+int main(){
+    system ("cls");
+    int a,b = 0,c = 1;
+
+    cout << "Program deret (+3,+5,+3,+5.....)\n";
+    cout << "ingin looping hingga deret ke berapa: ";
+    cin >> a;
+    
+    
+    while(b<=a){
+        cout << c << " ";
+        c+=5;   b+=1;
+        if (b == a) break;
+        cout << c << " ";
+        c+=3;   b+=1;
+    }
+
+}
