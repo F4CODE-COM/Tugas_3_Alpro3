8a5bacaf60aef880d90d326477f4d15e34777ef4
F4CODE-COM <febrianalkatammi21@gmail.com>
2026-04-15 22:04:27 +0700

    soal6.cpp

--- soal6.cpp
+++ soal6.cpp
@@ -1,1 +1,21 @@
+#include<conio.h>
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    int jumlah = 0;
+    
+    cout << "Masukkan bilangan: ";
+    cin >> n;
+    
+    for (int i = 2; i <= n; i = i + 2) {
+        jumlah = jumlah + (i * i);
+    }
+    
+    cout << "Jumlah semua kuadrat bilangan genap hingga " << n << ": \n";
+    cout << jumlah << endl;
+    
+    getch();
+    return 0;
+}
