0c858aaf40bc50f2c8f6895a5fa05a7cc3264f55
F4CODE-COM <febrianalkatammi21@gmail.com>
2026-04-15 22:00:11 +0700

    soal5.cpp

--- soal5.cpp
+++ soal5.cpp
@@ -1,1 +1,26 @@
+#include<conio.h>
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    int pembagi = 0;
+    
+    cout << "Masukkan bilangan: ";
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            pembagi++;
+        }
+    }
+    
+    if (pembagi == 2) {
+        cout << "Prima" << endl;
+    } else {
+        cout << "Bukan Prima" << endl;
+    }
+    
+    getch();
+    return 0;
+}
